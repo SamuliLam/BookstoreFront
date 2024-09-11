3cd81b21704a765381d4790dc6b14cac6976bd78
onboni <onni.luova@gmail.com>
2024-09-11 18:34:38 +0300

    Outlet otettu käyttöön

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
 import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import FilterPanel from './components/FilterPanel';
-import ProductGrid from './components/ProductGrid';
 import SortOptions from './components/SortOptions';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Book from './components/Books/Book'; // Import Book component
+import ProductGrid from './components/ProductGrid';
+import Book from './components/Books/Book';
+
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+            <SearchBar />
+            <div className="flex">
+                <FilterPanel />
+                <div className="flex-1 p-4">
+                    <SortOptions />
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const App = () => {
     return (
         <Router>
-            <div>
-                <Header />
-                <SearchBar />
-                <div className="flex">
-                    <FilterPanel />
-                    <div className="flex-1 p-4">
-                        <SortOptions />
-                        <Routes>
-                            <Route path="/books" element={<ProductGrid />} />
-                            <Route path="/book/:id" element={<Book />} />
-                        </Routes>
-                    </div>
-                </div>
-            </div>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="books" element={<ProductGrid />} />
+                    <Route path="book/:id" element={<Book />} />
+                </Route>
+            </Routes>
         </Router>
     );
 };
