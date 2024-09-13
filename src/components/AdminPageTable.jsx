6d14d1a7990a26eae41fd5c5f19d03f47937a66a
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-09-13 10:05:24 +0300

    Started admin panel.

--- src/components/AdminPageTable.jsx
+++ src/components/AdminPageTable.jsx
@@ -2,7 +2,13 @@ const AdminPageTable = ({ data }) => {
 
     return (
         <table className="table-auto w-full">
-
+            for (let i = 0; i < data.length; i++) {
+                <tr>
+                    for (let j = 0; j < data[i].length; j++) {
+                        <td className="border px-4 py-2">{data[i][j]}</td>
+                    }
+                </tr>
+            }
         </table>
     )
 }
