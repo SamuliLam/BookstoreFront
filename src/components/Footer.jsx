dd3dee13c21888c32aea173528d84c48f560a220
kismet <kismet1985@hotmail.fi>
2024-09-14 15:50:32 +0300

    footer muutokset

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 
 const Footer = () => {
   return (
-    <footer className={"flex flex-col items-center"}>
-        <p>© Contact us: online.bookstore@gmail.com</p>
-    </footer>
-    );
+      <footer className="flex flex-col justify-center items-center min-h-[50px]">
+          <p>© Contact us: online.bookstore@gmail.com</p>
+      </footer>
+  );
 }
 
 export default Footer;
