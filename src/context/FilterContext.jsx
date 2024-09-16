7a856c22e53055d04e5301e92ecd7f90efda4171
onboni <onni.luova@gmail.com>
2024-09-16 17:41:05 +0300

    Filter improvements

--- src/context/FilterContext.jsx
+++ src/context/FilterContext.jsx
@@ -1,13 +1,27 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import useFetchBooks from '../hooks/useFetchBooks';
 
 export const FilterContext = createContext();
 
 export const FilterProvider = ({ children }) => {
     const [selectedGenre, setSelectedGenre] = useState(null);
-    const [selectedPrice, setSelectedPrice] = useState(100); // Default to max price
+    const [selectedPrice, setSelectedPrice] = useState(100);
+    const [minPrice, setMinPrice] = useState(100);
+    const [maxPrice, setMaxPrice] = useState(100);
+    const { books, loading, error } = useFetchBooks();
+
+    useEffect(() => {
+        if (!loading && !error) {
+            const minBookPrice = Math.min(...books.map(book => book.price));
+            const maxBookPrice = Math.max(...books.map(book => book.price));
+            setMinPrice(minBookPrice);
+            setMaxPrice(maxBookPrice);
+            setSelectedPrice(maxBookPrice);
+        }
+    }, [books, loading, error]);
 
     return (
-        <FilterContext.Provider value={{ selectedGenre, setSelectedGenre, selectedPrice, setSelectedPrice }}>
+        <FilterContext.Provider value={{ selectedGenre, setSelectedGenre, selectedPrice, setSelectedPrice, minPrice, maxPrice }}>
             {children}
         </FilterContext.Provider>
     );
