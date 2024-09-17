dff6e0ed2a8c27d74449bc357e48d1e2606488f8
Stefanos Thomas <stefanos.thomas@metropolia.fi>
2024-09-17 12:10:25 +0300

    Items can be added to shopping cart and the state persists even if the page is refreshed.

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { FaShoppingCart, FaTimes } from "react-icons/fa";
+import React, {useState, useEffect, useRef} from 'react';
+import {FaShoppingCart, FaTimes} from "react-icons/fa";
+import ShoppingCartProductCard from "./ShoppingCartProductCard.jsx";
+import {useCartContext} from "../context/CartContext.jsx";
 
 const ShoppingCart = () => {
     const [isVisible, setIsVisible] = useState(false);
     const cartRef = useRef(null);
     const overlayRef = useRef(null);
-
+    const {cart} = useCartContext();
     const handleToggle = () => {
         setIsVisible(!isVisible);
     };
@@ -31,7 +33,7 @@ const ShoppingCart = () => {
     return (
         <div>
             <button onClick={handleToggle} className="text-3xl hover:text-blue-500">
-                <FaShoppingCart />
+                <FaShoppingCart/>
             </button>
             <div
                 className={`fixed inset-0 bg-gray-800 transition-opacity duration-300 ${isVisible ? 'opacity-50' : 'opacity-0 pointer-events-none'} z-40`}
@@ -40,9 +42,22 @@ const ShoppingCart = () => {
             <div
                 ref={cartRef}
                 className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 z-50 ${isVisible ? 'translate-x-0' : 'translate-x-full'}`}>
-                <button onClick={handleToggle} className="text-2xl hover:text-blue-500 absolute top-0 left-0 m-2">
-                    <FaTimes />
-                </button>
+                <span onClick={handleToggle} className="text-2xl hover:text-blue-500 cursor-pointer">
+                        <FaTimes/>
+                    </span>
+                <div>
+                    <h2 className="text-2xl font-bold p-4">Shopping Cart</h2>
+                    <div ref={overlayRef} className="overflow-y-auto h-96">
+                        {cart.length === 0 ? (
+                            <p className="p-4">No items in the cart</p>
+                        ) : (
+                            cart.map((book) => (
+                                <ShoppingCartProductCard key={book.book_id} title={book.title} price={book.price}
+                                                         image={book.image_url}/>
+                            ))
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,21 @@
-import React, { createContext, useContext, useState} from "react";
+import React, {createContext, useContext, useEffect, useState} from "react";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        try {
+            const item = localStorage.getItem('cart');
+            return item ? JSON.parse(item) : [];
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
 
     const addToCart = (book) => {
         setCart([...cart, book]);
