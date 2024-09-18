2145039bc95a0f9af28420106285e53d21d8b134
Stefanos Thomas <stefanos.thomas@metropolia.fi>
2024-09-18 15:28:25 +0300

    Shopping cart now displays total.

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import { FaShoppingCart, FaTimes } from "react-icons/fa";
+import React, {useEffect} from 'react';
+import {FaShoppingCart, FaTimes} from "react-icons/fa";
 import ShoppingCartProductCard from "./ShoppingCartProductCard.jsx";
-import { useCartContext } from "../context/CartContext.jsx";
+import {useCartContext} from "../context/CartContext.jsx";
 
 const ShoppingCart = () => {
-    const { cart, isVisible, handleToggle, handleClickOutside, cartRef, overlayRef } = useCartContext();
+    const {cart, isVisible, handleToggle, handleClickOutside, cartRef, overlayRef} = useCartContext();
 
     useEffect(() => {
         const overlayNode = overlayRef.current;
@@ -21,7 +21,7 @@ const ShoppingCart = () => {
     return (
         <div>
             <button onClick={handleToggle} className="text-3xl hover:text-blue-500">
-                <FaShoppingCart />
+                <FaShoppingCart/>
             </button>
             <div
                 className={`fixed inset-0 bg-gray-800 transition-opacity duration-300 ${isVisible ? 'opacity-50' : 'opacity-0 pointer-events-none'} z-40`}
@@ -29,15 +29,15 @@ const ShoppingCart = () => {
             ></div>
             <div
                 ref={cartRef}
-                className={`fixed top-0 right-0 h-full w-96 bg-white shadow-lg transform transition-transform duration-300 z-50 ${isVisible ? 'translate-x-0' : 'translate-x-full'}`}>
+                className={`fixed top-0 right-0 h-full w-72 bg-white shadow-lg transform transition-transform duration-300 z-50 ${isVisible ? 'translate-x-0' : 'translate-x-full'}`}>
                 <span onClick={handleToggle} className="text-2xl hover:text-blue-500 cursor-pointer">
-                    <FaTimes />
+                    <FaTimes/>
                 </span>
                 <div>
-                    <h2 className="text-2xl font-light text-sm p-4">Shopping Cart</h2>
-                    <div ref={overlayRef} className="flex-1 overflow-y-auto h-[90vh]">
+                    <h2 className="text-lg font-light p-4">Shopping Cart</h2>
+                    <div ref={overlayRef} className="flex-1 overflow-y-auto max-h-[80vh]">
                         {cart.length === 0 ? (
-                            <p className="p-4">No items in the cart</p>
+                            <p className="text-base font-light p-4">No items in the cart</p>
                         ) : (
                             cart.map((book) => (
                                 <ShoppingCartProductCard
@@ -45,6 +45,16 @@ const ShoppingCart = () => {
                                     book={book}
                                 />
                             ))
+                        )}
+                    </div>
+                    <div>
+                        {cart.length > 0 && (
+                            <div className="flex justify-between p-4">
+                                <p className="text-sm font-light">Total:</p>
+                                <p className="text-sm font-light">
+                                    {cart.reduce((acc, item) => acc + item.price * item.quantity, 0)}€
+                                </p>
+                            </div>
                         )}
                     </div>
                 </div>
