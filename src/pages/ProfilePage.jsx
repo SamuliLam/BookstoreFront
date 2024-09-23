0814d18609a59cb192626c9e16245ff59c93cdf6
onboni <onni.luova@gmail.com>
2024-09-23 13:26:40 +0300

    Dark mode order historyyn

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -160,9 +160,8 @@ const ProfilePage = () => {
         if (!order.orderItems) return null;
 
         return (
-            <div className="mt-4 bg-gray-50 p-4 rounded-lg dark:bg-gray-700">
-                <h4 className="text-lg font-semibold mb-2 dark:text-gray-200">Order Items:</h4>
-                <ul className="space-y-2">
+            <div className="mt-4 bg-gray-50 p-4 rounded-lg dark:bg-gray-800">
+                <ul className="space-y-1">
                     {order.orderItems.map((item, index) => (
                         <li key={index} className="flex justify-between items-center">
                         <span className="dark:text-gray-300">
@@ -285,7 +284,6 @@ const ProfilePage = () => {
                                             </th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">Date</th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">Total</th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">Status</th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">Details</th>
                                         </tr>
                                         </thead>
@@ -297,16 +295,6 @@ const ProfilePage = () => {
                                                     <td className="px-6 py-4 whitespace-nowrap dark:text-gray-300">{order.id}</td>
                                                     <td className="px-6 py-4 whitespace-nowrap dark:text-gray-300">{order.date}</td>
                                                     <td className="px-6 py-4 whitespace-nowrap dark:text-gray-300">${order.total.toFixed(2)}</td>
-                                                    <td className="px-6 py-4 whitespace-nowrap dark:text-gray-300">
-                                                        <span
-                                                            className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                                                                order.status === 'SUCCESSFUL' ? 'bg-green-100 text-green-800 dark:bg-green-800 dark:text-green-100' :
-                                                                    order.status === 'Processing' ? 'bg-yellow-100 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-100' :
-                                                                        'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300'
-                                                            }`}>
-                                                            {order.status}
-                                                        </span>
-                                                    </td>
                                                     <td className="px-6 py-4 whitespace-nowrap dark:text-gray-300">
                                                         <button
                                                             onClick={() => toggleOrderDetails(order.id)}
