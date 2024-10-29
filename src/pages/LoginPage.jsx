0c026c5a5b8a3611e0759535a4a49c6042b9e8eb
kismet <kismet1985@hotmail.fi>
2024-10-29 17:22:11 +0200

    i18 language localization

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useUserContext } from '../context/UserContext';
 import { logIn } from '../utils/api';
 import bookImage from '../assets/readbook.png';
 import { useNavigate } from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 const LoginPage = () => {
     const { login } = useUserContext();
@@ -11,6 +12,7 @@ const LoginPage = () => {
     const [error, setError] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
     const navigate = useNavigate();
+    const { t, i18n } = useTranslation();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -20,7 +22,7 @@ const LoginPage = () => {
             const { success, user, error: loginError } = await logIn({ email, password });
             if (success && user) {
                 login(user);
-                setSuccessMessage('Login successful!');
+                setSuccessMessage(t("loginSuccessLoginMessage"));
 
 
                 setTimeout(() => {
@@ -33,10 +35,10 @@ const LoginPage = () => {
                     }
                 }, 2000);
             } else {
-                setError(loginError || 'Login failed. Please check your credentials.');
+                setError(loginError || t('loginFailedMessage'));
             }
         } catch (err) {
-            setError('An unexpected error occurred. Please try again.');
+            setError(t('loginUnexpectedError'));
             console.error(err);
         }
     };
@@ -52,16 +54,16 @@ const LoginPage = () => {
             <div
                 className="w-[100vw] xl:w-1/2 flex flex-col justify-center items-center dark:bg-gray-900 dark:text-white">
                 <form onSubmit={handleSubmit} className="w-1/2">
-                    <h2 className="text-2xl font-light mb-6 text-center ">Sign In</h2>
+                    <h2 className="text-2xl font-light mb-6 text-center ">{t("loginPageSignIn")}</h2>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="email">
-                            Email
+                            {t("loginPageEmail")}
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="email"
                             type="email"
-                            placeholder="Email"
+                            placeholder={t("loginPageEmail")}
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             required
@@ -70,13 +72,13 @@ const LoginPage = () => {
                     <div className="mb-6">
                         <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white"
                                htmlFor="password">
-                            Password
+                            {t("loginPagePassword")}
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="password"
                             type="password"
-                            placeholder="Password"
+                            placeholder={t("loginPagePassword")}
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
@@ -89,20 +91,19 @@ const LoginPage = () => {
                             className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 underline"
                             href="/"
                         >
-                            Forgot password?
+                            {t("loginPageForgotPass")}
                         </a>
                     </div>
                     <button
                         className="dark:hover:bg-blue-300 bg-black text-white font-bold py-2 px-4 rounded w-full hover:bg-gray-700 focus:outline-none focus:shadow-outline dark:bg-blue-500"
                         type="submit"
                     >
-                        Sign In
+                        {t("loginPageSignIn")}
                     </button>
                     <div className="text-center mt-6">
-                        <p className="text-gray-600 text-sm dark:text-white">OR</p>
+                        <p className="text-gray-600 text-sm dark:text-white">{t("loginPageOr")}</p>
                         <a href="/signup"
-                           className="text-blue-500 hover:text-blue-800 font-bold dark:text-blue-400 dark:hover:text-blue-800">Sign
-                            up</a>
+                           className="text-blue-500 hover:text-blue-800 font-bold dark:text-blue-400 dark:hover:text-blue-800">{t("loginPageSignUp")}</a>
                     </div>
                 </form>
             </div>
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -3,8 +3,10 @@ import { useUserContext } from "../context/UserContext.jsx";
 import { handleSignUp, logIn } from "../utils/api.js";
 import bookImage from "../assets/readbook.png";
 import { useNavigate } from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 const SignupPage = () => {
+    const { t, i18n } = useTranslation();
     const { login } = useUserContext();
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
@@ -50,17 +52,17 @@ const SignupPage = () => {
             </div>
             <div className={"w-[100vw] xl:w-1/2 flex flex-col justify-center items-center dark:bg-gray-900"}>
             <form onSubmit={handleSubmit} className="w-1/2">
-                <h2 className="text-2xl font-light mb-6 text-center dark:text-white">Sign Up</h2>
+                <h2 className="text-2xl font-light mb-6 text-center dark:text-white">{t("loginPageSignUp")}</h2>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="first_name">
-                        First Name
+                        {t("signupPageFirstName")}
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="first_name"
                         type="text"
                         name={"first_name"}
-                        placeholder="First Name"
+                        placeholder={t("signupPageFirstName")}
                         value={formData.first_name}
                         onChange={handleChange}
                         required
@@ -68,14 +70,14 @@ const SignupPage = () => {
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="last_name">
-                        Last Name
+                        {t("signupPageLastName")}
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="last_name"
                         type="text"
                         name={"last_name"}
-                        placeholder="Last Name"
+                        placeholder={t("signupPageLastName")}
                         value={formData.last_name}
                         onChange={handleChange}
                         required
@@ -83,14 +85,14 @@ const SignupPage = () => {
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="email">
-                        Email
+                        {t("loginPageEmail")}
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="email"
                         type="email"
                         name={"email"}
-                        placeholder="Email"
+                        placeholder={t("loginPageEmail")}
                         value={formData.email}
                         onChange={handleChange}
                         required
@@ -98,14 +100,14 @@ const SignupPage = () => {
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="password">
-                        Password
+                        {t("loginPagePassword")}
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="password"
                         type="password"
                         name={"password"}
-                        placeholder="Password"
+                        placeholder={t("loginPagePassword")}
                         value={formData.password}
                         onChange={handleChange}
                         required
@@ -115,11 +117,11 @@ const SignupPage = () => {
                     className="dark:hover:bg-blue-300 dark:bg-white dark:text-black bg-black text-white font-bold py-2 px-4 rounded w-full hover:bg-blue-300 focus:outline-none focus:shadow-outline "
                     type="submit"
                 >
-                    Sign Up
+                    {t("loginPageSignUp")}
                 </button>
                 <div className="text-center mt-6">
-                    <p className="text-gray-600 text-sm">OR</p>
-                    <a href="/login" className="text-blue-500 hover:text-blue-800 font-bold dark:text-blue-400 dark:hover:text-blue-800">Sign in</a>
+                    <p className="text-gray-600 text-sm">{t("loginPageOr")}</p>
+                    <a href="/login" className="text-blue-500 hover:text-blue-800 font-bold dark:text-blue-400 dark:hover:text-blue-800">{t("loginPageSignIn")}</a>
                 </div>
             </form>
         </div>
